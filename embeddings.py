93d88e189135819e1fb42c5ddb1d221381358566
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-09-16 00:55:35 +0500

    Create embeddings.py

---
+++ embeddings.py
@@ -0,0 +1,47 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+import os
+import pinecone 
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import streamlit as st
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENV = os.getenv('PINECONE_ENVIRONMENT')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+
+directory = 'data'
+
+def doc_preprocessing(directory):
+    loader = DirectoryLoader(directory)
+    docs = loader.load()
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=0
+    )
+    docs_split = text_splitter.split_documents(docs)
+    return docs_split
+
+@st.cache_resource
+def embedding_db():
+    # we use the openAI embedding model
+    embeddings = OpenAIEmbeddings()
+    pinecone.init(
+        api_key=PINECONE_API_KEY,
+        environment=PINECONE_ENV
+    )
+    docs_split = doc_preprocessing(directory)
+    doc_db = Pinecone.from_documents(
+        docs_split, 
+        embeddings, 
+        index_name='mojix-index'
+    )
+    return doc_db
